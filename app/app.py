8b32918cbff7c12ae62c0a1d18a7f3016ffa01ab
kr3915 <ondrej.brdek@gmail.com>
2025-11-24 09:03:02 +0100

    added ig stats

--- app/app.py
+++ app/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+import scripts.github as gh
+from flask import render_template
+import scripts.instagram as ig
 app = Flask(__name__)
 
 @app.route("/")
 def show_text():
-           return "<p>cego</p>"
+           return render_template("dashboard/dashboard.html", 
+                                  github_stars=gh.get_stars(), 
+                                  github_followers=gh.get_github_followers(),
+                                  ig_followers=ig.get_ig_followers(),
+                                  ig_following=ig.get_ig_following())
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
